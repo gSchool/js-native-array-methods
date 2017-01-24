fe8a6a2328b5b166fc69214db98833409f21a46a
Bill Boughton <boughton.william@gmail.com>
2017-01-24 13:15:46 -0700

    fixed allOfOneType again

--- src/every.js
+++ src/every.js
@@ -22,19 +22,25 @@ function allNutritious (items) {
 // ]
 // Expected Output: false
 function allOfOneType (items) {
+
+  //working with Nick Sugar to work through his code step by step
+
     function oneType(item, _, collection) {
 
+      //creating a seperate forloop for the
        var arrayTest = false;
        if (Array.isArray(item)) {
          arrayTest = true;
        }
 
+       //taking theh current 'item' from the items.every and comparing //it to all parts of 'collection'
        for (var i = 0; i < collection.length; i++) {
          if (typeof item !== typeof collection[i]){
            return false;
          }
        }
 
+       //if the array test above created true, now go through each //elemet of the array and test it against the currently //selected items.every.  CHekcing to see if an item matches. 
        if (arrayTest === true) {
          for (var j = 0; j < collection.length; j++) {
            if ((Array.isArray(item))!==(Array.isArray(collection[j]))){
@@ -43,7 +49,7 @@ function allOfOneType (items) {
          }
        }
 
-       return true;
+       return true; //if none of the flags returned false, then all //items match
      }
      return items.every(oneType);
 
