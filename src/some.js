9c369484d6c109e4c806e832db25294d3c3849db
John Shepler <john.sheplerjr@gmail.com>
2017-01-27 15:19:26 -0700

    completed entire assessment

--- src/some.js
+++ src/some.js
@@ -3,13 +3,22 @@
 // Sample Input: [ 10, 20, 30, 40, -50 ]
 // Expected Output: true
 function anyAreNegative (numbers) {
-
+var isNeg = numbers.some(function(number){
+  return number < 0;
+});
+return isNeg;
 }
-
 // Sample Input: [ 'Once', 'upon', 'a', 'time', '' ]
 // Expected Output: true
 function includesFalseyValues (items) {
-
+var nope = items.some(function(item){
+if(item===null){
+  return true;
+}
+var result =  item == false || isNaN(item);
+return result;
+});
+return nope;
 }
 
 module.exports = { anyAreNegative, includesFalseyValues }
