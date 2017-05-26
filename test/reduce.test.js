faa8f673e0a1443cf83f3b7a8cbc89a35f5cf1ce
sperella <perella@gmail.com>
2017-05-26 14:47:15 -0600

    Update reduce.test.js

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -15,36 +15,4 @@ describe('flatten', function () {
     assert.deepEqual(result, expected)
   })
 
-  it('should return an empty array when the input is empty', function () {
-    const data = []
-    const expected = []
-    const result = flatten(data)
-
-    assert.deepEqual(result, expected)
-  })
-})
-
-describe('consolidate', function () {
-  it('should format the inputted matrix as an array of objects', function () {
-    const data = [
-      { phone: { home: '(555) 655-4555', cell: '(333) 655-4555' } },
-      { phone: { home: '(555) 455-5555', cell: '(333) 455-5555' } },
-      { phone: { home: '(555) 255-5555', cell: '(333) 255-5555' } }
-    ]
-    const expected = {
-      home: [ '(555) 655-4555', '(555) 455-5555', '(555) 255-5555' ],
-      cell: [ '(333) 655-4555', '(333) 455-5555', '(333) 255-5555' ]
-    }
-    const result = consolidate(data)
-
-    assert.deepEqual(result, expected)
-  })
-
-  it('should return an empty object when the input is empty', function () {
-    const data = []
-    const expected = {}
-    const result = consolidate(data)
-
-    assert.deepEqual(result, expected)
-  })
 })
