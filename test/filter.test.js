8a3724635020dcd9dbed95c6c6098c9e6656ce06
sperella <perella@gmail.com>
2017-05-26 14:53:37 -0600

    Update filter.test.js

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,17 +1,6 @@
 const { assert } = require('chai')
 const { onlyOdd, findById } = require('../src/filter')
 
-describe('onlyOdd', function () {
-  it('should return only the odd items', function () {
-    const data = [11,22,33,44]
-    const expected = [11,33]
-    const result = onlyOdd(data)
-
-    assert.deepEqual(result, expected)
-  })
-
-})
-
 describe('findById', function () {
   it('should return only the object with the given id', function () {
     const data = [
