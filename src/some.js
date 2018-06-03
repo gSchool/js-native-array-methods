8c19e5c976721fc490e29e7bf870c74614677a49
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-06-03 00:22:28 -0700

    complete challenge

--- src/some.js
+++ src/some.js
@@ -3,13 +3,17 @@
 // Sample Input: [ 10, 20, 30, 40, -50 ]
 // Expected Output: true
 function anyAreNegative (numbers) {
-
+  return numbers.some(number => {
+    return number<0;
+  })
 }
 
 // Sample Input: [ 'Once', 'upon', 'a', 'time', '' ]
 // Expected Output: true
 function includesFalseyValues (items) {
-  
+    return items.some((item) => {
+      return (!item);
+    });
 }
 
 module.exports = { anyAreNegative, includesFalseyValues }
